6969db0b5ca9b3db45aa845b84b04ccdc14d0f85
Erno Newman <ernonewman@gmail.com>
2020-09-01 20:06:40 +0100

    Part 17 - Jumping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed = 3.5f;
+    public float JumpingForce = 10f;
+
+    private bool canJump = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +26,21 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * Speed * Time.deltaTime;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            canJump = false;
+            GetComponent<Rigidbody>().AddForce(0, JumpingForce, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Floor")
+        {
+            canJump = true;
+            Debug.Log("hit the floor!");
         }
     }
 }
