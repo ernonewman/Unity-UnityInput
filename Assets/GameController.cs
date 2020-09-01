8164a8b11d1734b6c59c8014e76bcfeb5b872004
Erno Newman <ernonewman@gmail.com>
2020-09-01 20:33:49 +0100

    Part 19 - Cycling Cameras

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject[] GameCameras;
+
+    private int gameCameraIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        FocusOnCamera(gameCameraIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Jump");
+            ChangeCamera(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Move left");
+            ChangeCamera(-1);
+        }
+    }
+
+    void FocusOnCamera(int index)
+    {
+        for (int i = 0; i < GameCameras.Length; i++)
+        {
+            GameCameras[i].SetActive(i == index);
+        }
+    }
+
+    void ChangeCamera(int direction)
+    {
+        gameCameraIndex += direction;
+
+        if (gameCameraIndex >= GameCameras.Length)
+        {
+            gameCameraIndex = 0;
+        }
+        else if (gameCameraIndex < 0)
+        {
+            gameCameraIndex = GameCameras.Length - 1;
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("Move right");
-        }
+        FocusOnCamera(gameCameraIndex);
     }
 }
